725496439276868111894152a6d39f9a23dcc5cb
Ken Yoshihara <yshrkn@gmail.com>
2020-02-24 18:38:19 +0900

    implement css-loader and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,17 @@ module.exports = {
     filename: 'main.js',
     path: outputPath
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ // ローダーは逆順で順番に実行されることに注意
+          'style-loader',
+          'css-loader'
+        ] 
+      }
+    ]
+  },
   devServer: {
     contentBase: outputPath,
   }
